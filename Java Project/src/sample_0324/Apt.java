4e873b56179f3b71dff4d840cd518b57b30981af
Java2-2012034229 <juhun08251@naver.com>
2016-03-24 20:45:01 +0900

    3월 24일 제발되라

---
+++ Java Project/src/sample_0324/Apt.java
@@ -0,0 +1,24 @@
+package sample_0324;
+
+public class Apt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=9;i>=1;i--){
+			for(int j=1;j<9;j++){
+				if(i>=6 && j>=6){
+					break;
+				}
+				System.out.print(i+"0"+j);
+				System.out.print(" ");
+				
+				
+			}
+			System.out.println(" ");
+		
+		}
+		
+	}
+
+}
